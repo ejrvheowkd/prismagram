88a6a7a052c85bf07cc2f0b5b1a181f29bbdc85a
ejrvheowkd <hoao1313@naver.com>
2020-07-07 18:53:00 +0900

    3.3 sendMail

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,35 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import mg from 'nodemailer-mailgun-transport';
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+
+export const sendMail = email => {
+  const options = {
+    auth: {
+      api_key: process.env.MAILGUN_API,
+      domain: process.env.MAILGUN_DOMAIN
+      
+    }
+  };
+  const client = nodemailer.createTransport(mg(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (adress, secret) => {
+  const email = {
+    from: "hoao1313@prismagram.com",
+    to: 'hoao1313@naver.com',
+    subject: "🔒Login Secret for Prismagram🔒",
+    html: `Hello! Your login secret it ${secret}.<br/>Copy paste on the app/website to log in`
+  };
+  return sendMail(email);
 };
