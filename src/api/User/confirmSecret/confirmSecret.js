1fa6726d056189f9d0df86069252be4d8d2a0d27
ejrvheowkd <hoao1313@naver.com>
2020-07-16 01:31:30 +0900

    #3.11 editUser seeUser Resolver

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -8,7 +8,11 @@ export default {
         const user = await prisma.user({email});//prisma에서 email을 인자로 사용자를 가져온다
         if(user.loginSecret=== secret){//if user.loginSecret와 secret이 같다면
         //generateToken함수를 실행해서 token에 할당.user.id를 함수의 인자로 입력해야 함
-            return generateToken(user.id);//jwt가 id를 암호화해서 토큰을 만들어 줘
+        await prisma.updateUser({
+            where:{id:user.id},data:{
+            loginSecret:''
+        }})    
+        return generateToken(user.id);//jwt가 id를 암호화해서 토큰을 만들어 줘
         } else {
             throw Error("Wrong email.secret conviation");//같지않으면 에러전달
         }            
