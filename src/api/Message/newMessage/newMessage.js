3d547b5fd50c5949e6b0120ed7c9bddb7d3ff046
ejrvheowkd <hoao1313@naver.com>
2020-08-07 01:35:00 +0900

    #3.28 newMessage Subscription part Two

---
+++ src/api/Message/newMessage/newMessage.js
@@ -0,0 +1,22 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default{
+    Subscription: {
+        newMessage: {
+            subscribe: (_,args) => {
+                const {roomId}  = args;
+                return prisma.$subscribe.message({
+                    AND:[
+                        {mutation_in:"CREATED"},
+                        {
+                            node:{
+                                room:{id:roomId}
+                            }
+                        }
+                    ]
+                });
+            },
+            resolve: payload=>payload//payload는 새로운것이 막 변경되면 리턴하는거다
+        }
+    }
+};
